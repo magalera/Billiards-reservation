3771f518bec35e588e4d0f1ec6095453218a9eb2
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-11-01 11:38:46 +0100

    Add ReservationDto

--- src/main/java/com/billiards/billiardsreservation/domain/ReservationDto.java
+++ src/main/java/com/billiards/billiardsreservation/domain/ReservationDto.java
@@ -2,18 +2,16 @@ package com.billiards.billiardsreservation.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ReservationDto {
-    private Long id;
+    private Long reservationId;
     private LocalDate date;
     private LocalTime time;
     private ReservationType type;
