29cb63c74b6a59c819905fc1c2b162e6727d0ee6
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-11-01 11:40:58 +0100

    Add ReservationRepository

--- src/main/java/com/billiards/billiardsreservation/repository/ReservationRepository.java
+++ src/main/java/com/billiards/billiardsreservation/repository/ReservationRepository.java
@@ -4,6 +4,11 @@ import com.billiards.billiardsreservation.domain.Reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Set;
+
+@Transactional
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Set<Reservation> findAll();
 }
