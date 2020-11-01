4f9e3fa990834e1f4cb5ce3fe6624a3d9d6ccda2
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-11-01 10:58:25 +0100

    Add ReservationController

--- src/main/java/com/billiards/billiardsreservation/controller/ReservationController.java
+++ src/main/java/com/billiards/billiardsreservation/controller/ReservationController.java
@@ -1,38 +1,45 @@
 package com.billiards.billiardsreservation.controller;
 
+import com.billiards.billiardsreservation.domain.Reservation;
 import com.billiards.billiardsreservation.domain.ReservationDto;
-import com.billiards.billiardsreservation.domain.ReservationType;
+import com.billiards.billiardsreservation.mapper.ReservationMapper;
+import com.billiards.billiardsreservation.service.ReservationDbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.HashSet;
 import java.util.Set;
 
 @RestController
 @RequestMapping("v1/reservation")
 public class ReservationController {
+    @Autowired
+    private ReservationDbService service;
+    @Autowired
+    private ReservationMapper mapper;
 
     @PostMapping(value = "create")
-    public void create(ReservationDto reservationDto){
+    public void create(@RequestBody ReservationDto reservationDto) {
+        service.save(mapper.mapToReservation(reservationDto));
     }
 
-    @GetMapping(value = "get")
-    public ReservationDto get(Long id){
-        return new ReservationDto(1L, LocalDate.now(), LocalTime.MIDNIGHT, ReservationType.TABLE);
+    @GetMapping(value = "get/{id}")
+    public ReservationDto get(@PathVariable Long id) throws ReservationNotFoundException {
+        return mapper.mapToReservationDto(service.get(id).orElseThrow(ReservationNotFoundException::new));
     }
 
     @GetMapping(value = "get_all")
-    public Set<ReservationDto>getAll(){
-        return new HashSet<>();
+    public Set<ReservationDto> getAll() {
+        return mapper.mapToReservationsDto(service.getAllReservations());
     }
 
     @PutMapping(value = "edit")
-    public ReservationDto edit(ReservationDto reservationDto){
-        return new ReservationDto(1L, LocalDate.now(), LocalTime.now(), ReservationType.BILLIARDS);
+    public ReservationDto edit(@RequestBody ReservationDto reservationDto) {
+        Reservation reservation = service.save(mapper.mapToReservation(reservationDto));
+        return mapper.mapToReservationDto(reservation);
     }
 
     @DeleteMapping(value = "cancel")
-    public void cancel(Long id){
+    public void cancel(@RequestBody Long id) {
+        service.delete(id);
     }
 }
